f0b17bced6ad6390bbf64c09608b38360074edc1
Dexter0900 <preet451383@gmail.com>
2025-06-26 18:58:51 +0530

    project initialized

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1>Welcome to SkillBridge</h1>
+      <p>Your React + Vite project is ready.</p>
+    </div>
+  );
+}
+
+export default App;
